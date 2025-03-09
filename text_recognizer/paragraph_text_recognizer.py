37e2d88944a8b891c2d354d36d30d5032ba4a2c8
Merhawi <merhawitsegay007@gmail.com>
2025-03-09 23:32:50 +0000

    adjust pre-commit changes

--- text_recognizer/paragraph_text_recognizer.py
+++ text_recognizer/paragraph_text_recognizer.py
@@ -1,7 +1,7 @@
 """
 Takes an image and returns all the text in it, by first segmenting the image with LineDetector, then extracting crops
 of the image corresponding to the line regions, and running each line region crop through LinePredictor.
-""" 
+"""
 from typing import List, Tuple, Union
 import cv2
 import numpy as np
@@ -29,7 +29,7 @@ class ParagraphTextRecognizer:
             image = image_or_filename
 
         line_region_crops = self._get_line_region_crops(image=image)
-        print([a.shape for a in line_region_crops]) 
+        print([a.shape for a in line_region_crops])
         prepared_line_region_crops = [
             self._prepare_image_for_line_predictor_model(image=crop)
             for crop in line_region_crops
@@ -40,7 +40,7 @@ class ParagraphTextRecognizer:
             for crop in prepared_line_region_crops
         ]
         return ' '.join(line_region_strings), line_region_crops
-    
+
     def _get_line_region_crops(self, image: np.ndarray, min_crop_len_factor: float= 0.02)->List[np.ndarray]:
         """Find all the line regions in square image and crop them out and return them."""
         prepared_image, scale_down_factor = self._prepare_image_for_line_detector_model(image)
@@ -54,9 +54,9 @@ class ParagraphTextRecognizer:
             image[y:y+h, x:x+w]
             for x, y, w, h in bounding_boxes_xywh
             if w >= min_crop_length and h >= min_crop_length
-        ] 
+        ]
         return line_region_crops
-    
+
     def _prepare_image_for_line_detector_model(self, image:np.ndarray) -> Tuple[np.ndarray, float]:
         """Convert uint8 image to float image with black background with shape self.line_detector_model.image_shape."""
         resized_image, scale_down_factor = _resize_image_for_line_detector_model(
@@ -65,7 +65,7 @@ class ParagraphTextRecognizer:
         )
         resized_image = (1. - resized_image / 255).astype('float32')
         return resized_image, scale_down_factor
-    
+
     def _prepare_image_for_line_predictor_model(self, image: np.ndarray) -> np.ndarray:
         """
         Convert uint8 image to float image with black background with shape self.line_predictor_model.image_shape
@@ -78,7 +78,7 @@ class ParagraphTextRecognizer:
         pad_width = ((0, expected_shape[0] -scaled_image.shape[0]), (0, expected_shape[1] - scaled_image.shape[1]))
         padded_image = np.pad(scaled_image, pad_width=pad_width, mode='constant', constant_values=255)
         return 1 - padded_image / 255
-    
+
 def _find_line_bounding_boxes(line_segmentation: np.ndarray):
     """Given a line segmentation, find bounding boxes for connected-component regions corresponding to non-0 labels."""
 
@@ -87,9 +87,9 @@ def _find_line_bounding_boxes(line_segmentation: np.ndarray):
         line_activation_image = (line_activation_image * 255).astype('uint8')
         line_activation_image = cv2.threshold(line_activation_image, 0.5, 1, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
-        bounding_cnts,_ = cv2.findContours(line_activation_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
+        bounding_cnts,_ = cv2.findContours(line_activation_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         return np.array([cv2.boundingRect(cnt) for cnt in bounding_cnts])
-    
+
     bboxes_xywh = np.concatenate([
         _find_line_bounding_boxes_in_channel(line_segmentation[:,:,i])
         for i in [1,2]
